b3f9a4f3eea8e6548b83cfa6c57c898d8cc4f68d
easchou <easchou@gmail.com>
2021-07-21 10:43:49 +0800

    Day5

---
+++ eason.el
@@ -0,0 +1,10 @@
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/lisp") 
+(require 'init-packages)
+(require 'init-ui)
+(require 'init-better-defaults)
+(require 'init-keybindings)
+(require 'init-org)
+(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
+(load-file custom-file)
